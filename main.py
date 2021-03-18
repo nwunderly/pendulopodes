e0f7d087473a4904cc2928a5a77915bd79bec900
nwunderly <48489521+nwunderly@users.noreply.github.com>
2021-03-18 00:40:12 -0400

    holy fuck, this is already so cool

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from pendulopodes.dynamics import system_single_simple_pendulum, polar_to_inerti
 from pendulopodes.diffeq import ode45_single_pendulum
 
 
-T_SPAN = [0, 100]
+T_SPAN = [0, 1]
 
 
 def vector(s):
@@ -25,14 +25,34 @@ def parse():
     return parser.parse_args()
 
 
+def gradient_for(count):
+    cmap = plt.get_cmap('rainbow')
+    step = 1.0/count
+
+    # invert the color list so it's red -> purple, might change later
+    return [cmap(0 + i*step) for i in range(count)][::-1]
+
+
 def plot(theta):
+    # use every other point to show spacing more clearly
+    theta = theta[::2]
+
     fig = plt.figure()
     ax = fig.add_subplot(111, title="Pendulum path")
+    ax.axis('equal')
 
     x, y = polar_to_inertial(1, theta)
 
-    ax.plot(y, -x)
+    # origin
     ax.plot(0, 0, 'ro')
+
+    n_points = len(theta)
+    grad = gradient_for(n_points)
+
+    # plot path
+    for i in range(n_points):
+        ax.plot(y[i], -x[i], color=grad[i], marker='+')
+
 
     plt.show()
 
--- pendulopodes/diffeq.py
+++ pendulopodes/diffeq.py
@@ -6,7 +6,7 @@ TOLERANCE = 10**-13
 
 def ode45_single_pendulum(system, t_span, theta0, theta_dot0):
     y0 = [theta0, theta_dot0]
-    soln = solve_ivp(system, t_span, y0, atol=TOLERANCE, rtol=TOLERANCE, first_step=1)
+    soln = solve_ivp(system, t_span, y0, atol=TOLERANCE, rtol=TOLERANCE, first_step=0.0001)
 
     y = soln.y
     t = soln.t
